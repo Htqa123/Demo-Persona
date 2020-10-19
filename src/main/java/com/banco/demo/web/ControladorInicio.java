3b2333ce9c94532af6797912645db9d672e7d2bb
hguruena4@misena.edu.co <hguruena4@misena.edu.co>
2020-10-19 11:59:09 -0500

    realizando logica del servicio getMaaping y getPost

--- src/main/java/com/banco/demo/web/ControladorInicio.java
+++ src/main/java/com/banco/demo/web/ControladorInicio.java
@@ -1,11 +1,13 @@
 package com.banco.demo.web;
 
+import com.banco.demo.domain.Persona;
 import com.banco.demo.servicio.PersonaServicio;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -19,6 +21,16 @@ public class ControladorInicio {
         log.info("ejecutando el controlador MVC");
         model.addAttribute("personas", personas);
         return ("index");
+    }
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaServicio.guardar(persona);
+        return "redirect:/";
     }
 
 }
